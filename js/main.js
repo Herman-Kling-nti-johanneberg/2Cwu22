9c660a52f20f1579ad6a2f8ceecbeef52c818d86
Herman-Kling-nti-johanneberg <herman.kling@elev.ga.ntig.se>
2022-03-28 22:56:33 +0200

    v.12.2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-/*alert("hej")*/
+let layermenu = document.querySelector(".fadelayermenu").addEventListener("click",showmenu)
+
+let buttonmenu = document.querySelector(".menubutton").addEventListener("click",showmenu)
+
+function showmenu(){
+    let menu = document.querySelector("nav.sidemenu");
+    menu.classList.toggle("showmenu");
+
+    let layermenu = document.querySelector(".fadelayermenu");
+    layermenu.classList.toggle("visible")
+}
+
+
+
+let buttonteam = document.querySelector(".teambutton").addEventListener("click",showteam)
+
+let layerteam = document.querySelector(".fadelayerteam").addEventListener("click",showteam)
+
+function showteam(){
+    let team = document.querySelector("nav.sideteam");
+    team.classList.toggle("showteam");
+
+    let layermenu = document.querySelector(".fadelayerteam");
+    layermenu.classList.toggle("visible")
+}
